f56d9631fa5bf0b43763ef4e9f654905ff1b8d03
Adam <adamgjohnson92@gmail.com>
2023-06-24 19:02:57 -0700

    included the user text input to the generateLogo function and adjusted the coordinates for the text

--- generateLogo.js
+++ generateLogo.js
@@ -1,14 +1,14 @@
 function generateLogo(data) {
     return `<svg 
     viewbox='0 0 400 300'
-    xmlns='http://www.w3.org/2000/svg'
     width='300' height ='200'
     stroke='${data.shapeColor}'
     fill='${data.shapeColor}'>
     <circle cx='150' cy='150' r='150' />
+    <text font-size='6em' x='75' y='175' fill='${data.textColor}'> ${data.text}</text>
     </svg>
         `
 };
 
 
-    module.exports = generateLogo;
+module.exports = generateLogo;
