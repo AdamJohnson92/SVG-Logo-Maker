fbe2f6f61b205c5efe91fb7cfb97bef2ac5816b1
Adam <adamgjohnson92@gmail.com>
2023-06-26 18:07:19 -0700

    Included constructors into the generateLogo.js

--- generateLogo.js
+++ generateLogo.js
@@ -1,35 +1,39 @@
-// const shapesJS = require('./lib/shapes')
+const ShapeCode = require('./lib/shapes')
+
 
 function generateLogo(data) {
     if (data.shape === 'circle'){
+        const circleObj = new ShapeCode 
 
     return `<svg 
     viewbox='0 0 400 300'
     width='300' height ='200'
     stroke='${data.shapeColor}'
     fill='${data.shapeColor}'>
-    <circle cx='150' cy='150' r='150' />
+    ${circleObj.circleCode()}
     <text font-size='6em' x='75' y='175' fill='${data.textColor}'> ${data.text}</text>
     </svg>
         `
 } else if (data.shape === 'triangle'){
+    const triangleObj = new ShapeCode 
 
     return `<svg 
     viewbox='0 0 400 300'
     width='300' height ='200'
     stroke='${data.shapeColor}'
     fill='${data.shapeColor}'>
-    <polygon points='0,300 150,50 300, 300' />
+    ${triangleObj.triangleCode()}
     <text font-size='5em' x='75' y='250' fill='${data.textColor}'> ${data.text}</text>
     </svg>
         `
 } else {
+    const squareObj = new ShapeCode
     return `<svg 
     viewbox='0 0 400 300'
     width='300' height ='200'
     stroke='${data.shapeColor}'
     fill='${data.shapeColor}'>
-    <polygon points='0,300 300,300 300,0 0,0' />
+    ${squareObj.squareCode()}
     <text font-size='5em' x='75' y='235' fill='${data.textColor}'> ${data.text}</text>
     </svg>
         `
